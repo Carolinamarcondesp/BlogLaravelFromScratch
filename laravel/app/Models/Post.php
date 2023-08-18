1ecbf1e6dbebf00ace1978ab94b6442c06c4bf5f
Carolina <carolinamarcondesp@gmail.com>
2023-08-18 15:05:56 +0100

    FixConfusingEloquentQueryBug

--- laravel/app/Models/Post.php
+++ laravel/app/Models/Post.php
@@ -20,9 +20,9 @@ class Post extends Model
         //later on if I have any other filter to the posts add them here
 
         $query->when($filters['search'] ?? false, fn($query, $search) =>
-            $query
-            ->where('title', 'like', '%' . $search . '%')
-            ->orWhere('body', 'like', '%' . $search . '%'));
+            $query->where(fn ($query) =>
+                $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%')));
 
         $query->when($filters['category'] ?? false, fn ($query, $category) =>
             $query->whereHas('category', fn ($query) =>
--- laravel/resources/views/components/category-dropdown.blade.php
+++ laravel/resources/views/components/category-dropdown.blade.php
@@ -18,9 +18,9 @@
         {{--  {{ isset($currentCategory) && $currentCategory->is($category)? 'bg-blue-500 text-white' : '' }}--}}
         {{--  ? 'bg-blue-500 text-white' : '' }}--}}
         <x-dropdown-item
-            href="?category={{$category->slug}}"
+            href="/?category={{$category->slug}}&{{ http_build_query(request()->except('category')) }}"
             {{-- :active="isset($currentCategory) && $currentCategory->is($category)" --}}
-            :active="request()->is('categories/' . $category->slug)"
+            :active='request()->is("categories/.$category->slug")'
         >{{ ucwords($category->name) }}</x-dropdown-item>
 
     @endforeach
