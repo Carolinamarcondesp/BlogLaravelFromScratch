20cb9a529a86a63c46cf177507c07bc3213ff01f
Carolina <carolinamarcondesp@gmail.com>
2023-08-21 16:34:27 +0100

    BuildLoginPage

--- laravel/app/Http/Controllers/SessionsController.php
+++ laravel/app/Http/Controllers/SessionsController.php
@@ -4,8 +4,38 @@ namespace App\Http\Controllers;
 
 
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        //validate the request
+
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        //attempt to authenticate and login the user based on the provided credentials
+        if(auth()->attempt($attributes)){
+            session()->regenerate(); //to avoid session fixation attacks
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        //auth failed
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+        /*return  back()
+            ->withInput()
+            ->withErrors(['email' => 'Your provided credentials could not be verified.']);*/
+
+    }
     public function destroy()
     {
         auth()->logout();
