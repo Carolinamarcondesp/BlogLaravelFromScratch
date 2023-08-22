1dda17a515cffb62ac426d94539559ec4b34694b
Carolina <carolinamarcondesp@gmail.com>
2023-08-22 10:59:00 +0100

    WriteMarkupforPostComment

---
+++ laravel/resources/views/components/post-comment.blade.php
@@ -0,0 +1,21 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0 mt-1">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis ligula a diam tincidunt,
+            vitae mattis tellus lacinia. Sed viverra ante tempor, aliquam lacus imperdiet, semper ex.
+            Suspendisse sed magna sit amet eros tempor sagittis.
+            Aliquam nec massa et mauris lobortis vestibulum at tempus arcu.
+            Ut vitae nulla hendrerit, ultrices nisi at, porta eros. Nullam sit amet lobortis urna.
+            Vestibulum quis nisi vel orci mollis pulvinar at in enim.
+        </p>
+    </div>
+</article>
