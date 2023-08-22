0304492582961d7921bfba5818d79177218e56dd
Carolina <carolinamarcondesp@gmail.com>
2023-08-22 13:18:08 +0100

    CommentSectionDynamic

--- laravel/app/Models/Post.php
+++ laravel/app/Models/Post.php
@@ -19,37 +19,44 @@ class Post extends Model
         // $query is a build-in laravel query build
         //later on if I have any other filter to the posts add them here
 
-        $query->when($filters['search'] ?? false, fn($query, $search) =>
-            $query->where(fn ($query) =>
-                $query->where('title', 'like', '%' . $search . '%')
-                ->orWhere('body', 'like', '%' . $search . '%')));
+        $query->when(
+            $filters['search'] ?? false,
+            fn($query, $search) => $query->where(fn($query) => $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%'))
+        );
 
-        $query->when($filters['category'] ?? false, fn ($query, $category) =>
-            $query->whereHas('category', fn ($query) =>
-                $query->where('slug', $category)));
-                //eloquent tip: speak out loud to avoid confusion
-                //so we are dealing with posts
-                //give me the ones where they have (whereHas) a category
-                //specifically where the category's slug matches what user requested ($category)
+        $query->when(
+            $filters['category'] ?? false,
+            fn($query, $category) => $query->whereHas('category', fn($query) => $query->where('slug', $category))
+        );
+        //eloquent tip: speak out loud to avoid confusion
+        //so we are dealing with posts
+        //give me the ones where they have (whereHas) a category
+        //specifically where the category's slug matches what user requested ($category)
 
-        $query->when($filters['author'] ?? false, fn ($query, $author) =>
-        $query->whereHas('author', fn ($query) =>
-        $query->where('username', $author)));
-        }
-    public function category(){
+        $query->when(
+            $filters['author'] ?? false,
+            fn($query, $author) => $query->whereHas('author', fn($query) => $query->where('username', $author))
+        );
+    }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+
+    public function category()
+    {
         //relationships types - hasOne, hasMany, belongsTo, belongsToMany
 
         return $this->belongsTo(Category::class);
-
     }
 
-    public function author(){
-
+    public function author()
+    {
         return $this->belongsTo(User::class, 'user_id');
     }
-
-
 
 
 }
--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -28,6 +29,8 @@ class DatabaseSeeder extends Seeder
         Post::factory(20)->create([
             'user_id' => $user->id
         ]);
+
+        Comment::factory(5)->create();
 
        /* $personal = Category::create([
             'name' => 'Personal',
