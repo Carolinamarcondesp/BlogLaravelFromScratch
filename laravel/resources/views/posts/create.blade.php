a2c1883f88fd736d1972e6c093559065ebc3a83b
Carolina <carolinamarcondesp@gmail.com>
2023-08-25 12:04:53 +0100

    extendTheAdminLayout

--- laravel/resources/views/posts/create.blade.php
+++ laravel/resources/views/posts/create.blade.php
@@ -5,86 +5,15 @@
             <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
 
-                <div class="mb-6">
-                    <label for="title" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Title
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full rounded"
-                           type="text"
-                           name="title"
-                           id="title"
-                           value="{{old('title')}}"
-                           required
-                    >
-                    @error('title')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
+                <x-form.input name="title"/>
+                <x-form.input name="slug"/>
+                <x-form.input name="thumbnail" type="file"/>
+                <x-form.textarea name="excerpt"/>
+                <x-form.textarea name="body"/>
 
-                <div class="mb-6">
-                    <label for="slug" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Slug
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full rounded"
-                           type="text"
-                           name="slug"
-                           id="slug"
-                           value="{{old('slug')}}"
-                           required
-                    >
-                    @error('slug')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
+                <x-form.field>
+                    <x-form.label name="category"/>
 
-                <div class="mb-6">
-                    <label for="thumbnail" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Thumbnail
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full rounded"
-                    type="file"
-                    name="thumbnail"
-                    id="thumbnail"
-                    required
-                    >
-                    @error('thumbnail')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-
-                <div class="mb-6">
-                    <label for="excerpt" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Excerpt
-                    </label>
-                    <textarea class="border border-gray-400 p-2 w-full rounded"
-                              type="text"
-                              name="excerpt"
-                              id="excerpt"
-                              required
-                    >{{old('excerpt')}}</textarea>
-                    @error('excerpt')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror()
-                </div>
-                <div class="mb-6">
-                    <label for="body" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Body
-                    </label>
-                    <textarea class="border border-gray-400 p-2 w-full rounded"
-                              type="text"
-                              name="body"
-                              id="body"
-                              required
-                    >{{old('body')}}</textarea>
-                    @error('body')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror()
-                </div>
-                <div class="mb-6">
-                    <label for="category_id" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Category
-                    </label>
                     <select name="category_id" id="category_id">
                         @foreach(App\Models\Category::all() as $category)
                             <option
@@ -92,16 +21,14 @@
                                 {{ old('$category_id') == $category->id ? 'selected' : '' }}
                             >{{ucwords($category->name)}}</option>
                         @endforeach
-
                     </select>
-                    @error('category')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror()
-                </div>
+                    <x-form.error name="category"/>
+
+                </x-form.field>
 
 
                 <div class="mb-6 flex justify-end">
-                    <x-submit-button>Publish</x-submit-button>
+                    <x-form.button>Publish</x-form.button>
                 </div>
 
             </form>
